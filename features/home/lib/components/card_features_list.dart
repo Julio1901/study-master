c42021e674b6833f316bd0eb44f5007894fde294
Julio Cesar <js.cesar42@gmail.com>
2024-06-21 09:23:51 -0300

    style: Title container with bluer effect created into home feature card and image added into the same card

---
+++ features/home/lib/components/card_features_list.dart
@@ -0,0 +1,81 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CardFeaturesList extends StatelessWidget {
+  String title;
+  String description;
+  IconData icon;
+
+  CardFeaturesList(
+      {super.key,
+      required this.title,
+      required this.description,
+      required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 190,
+      height: 186,
+      decoration: const BoxDecoration(
+          gradient: RadialGradient(
+            colors: [
+              Colors.white, // Cor no centro
+              Color.fromARGB(255, 236, 191, 97), // Cor nas bordas
+            ],
+            stops: [0.0, 1.0], // Define os pontos de parada do degradê
+            center: Alignment.center, // Centro do degradê
+            radius: 0.6, // Raio do degradê
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(16))),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                child: Container(
+                  width: 143,
+                  height: 30,
+                  decoration: BoxDecoration(
+                     borderRadius: const BorderRadius.all(Radius.circular(16)),
+                     border: Border.all(
+                      color: Colors.white.withOpacity(0.3),
+                      width: 1.5,
+                    ),
+                    //  color: Colors.black12
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        title,
+                        style: const TextStyle(
+                            fontSize: 20,
+                            color: Color.fromARGB(255, 24, 147, 240),
+                            fontFamily: 'Montserrat',
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.w600
+                            ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Image.asset('assets/images/pomodoro-icon-transparent.png', width: 140,))
+          // Text(description),
+          // Icon(icon)
+        ],
+      ),
+    );
+  }
+}
--- features/home/lib/screens/home_screen.dart
+++ features/home/lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:home/components/card_features_list.dart';
 import 'package:home/screens/subject_details_screen.dart';
 import 'package:study_master/common/strings.dart';
 import 'package:study_master/data/user_defaults.dart';
@@ -17,78 +20,126 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 15, right: 15),
-          child: Container(
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '${AppStrings.getString('welcome-message')} ${userDefaults.userName}',
-                  style: TextStyle(
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 15, right: 15),
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '${AppStrings.getString('welcome-message')} ${userDefaults.userName}',
+                style: TextStyle(
                     fontSize: 15,
                     color: Color.fromARGB(255, 89, 88, 88),
                     fontFamily: 'Montserrat',
-                    fontStyle: FontStyle.normal 
-                  ),
+                    fontStyle: FontStyle.normal),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 5),
+                child: Text(
+                  AppStrings.getString('manage-your-study'),
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 5),
-                  child: Text(AppStrings.getString('manage-your-study')
-                  , style: TextStyle(
-                    fontSize: 25,
-                    fontFamily: 'Montserrat',
-                    fontWeight: FontWeight.bold
-                  ),),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30, bottom: 15),
+                child: Text(
+                  AppStrings.getString('study-topics'),
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontFamily: 'Montserrat',
+                      fontStyle: FontStyle.normal),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 30, bottom: 15),
-                  child: Text(AppStrings.getString('study-topics'), style: TextStyle(
-                    fontSize: 22,
-                    fontFamily: 'Montserrat',
-                    fontStyle: FontStyle.normal 
-                  ),),
-                ),
-                Container(
+              ),
+              Container(
                   width: double.infinity,
                   height: 200,
                   child: Consumer<SubjectListViewModel>(
-                    builder: (BuildContext context, SubjectListViewModel subjectListViewModel, Widget? widget) {
-                      return ListView(
+                    builder: (BuildContext context,
+                        SubjectListViewModel subjectListViewModel,
+                        Widget? widget) {
+                      return ListView.separated(
                         scrollDirection: Axis.horizontal,
-                        children: subjectListViewModel.subjects.map((subject) {
-                          return CardWithBorderRounded(
-                            title: subject.title,
-                            subjectField: subject.topicName,
-                            imageName: subject.imagePath,
-                          );
-                        }).toList(),
+                        itemCount: subjectListViewModel.subjects.length,
+                        separatorBuilder: (context, _) => SizedBox(width: 5),
+                        itemBuilder: (context, index) => CardWithBorderRounded(
+                            title: subjectListViewModel.subjects[index].title,
+                            subjectField:
+                                subjectListViewModel.subjects[index].topicName,
+                            imageName:
+                                subjectListViewModel.subjects[index].imagePath),
                       );
                     },
-                  )
-                ),
-                Consumer<SubjectListViewModel>(
-                  builder: (BuildContext context, SubjectListViewModel subjectListViewModel, Widget? widget){
-                    return ElevatedButton(onPressed: () {
-                      subjectListViewModel.addSubject(Subject(title: 'Data Struct', topicName: 'Software Engineering', imagePath: 'software-logo.png'));
-                }, child: Text('Press me'));
+                  )),
+              Consumer<SubjectListViewModel>(
+                builder: (BuildContext context,
+                    SubjectListViewModel subjectListViewModel, Widget? widget) {
+                  return ElevatedButton(
+                      onPressed: () {
+                        subjectListViewModel.addSubject(Subject(
+                            title: 'Data Struct',
+                            topicName: 'Software Engineering',
+                            imagePath: 'software-logo.png'));
+                      },
+                      child: Text('Press me'));
+                },
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return SubjectDetailsScreen();
+                    }));
                   },
+                  child: Text('Go to details screen')),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: CardFeaturesList(
+                          title: 'Pomodoro',
+                          description:
+                              'Use this Pomodoro to improve your focus',
+                          icon: Icons.alarm_rounded),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: CardFeaturesList(
+                          title: 'Pomodoro',
+                          description:
+                              'Use this Pomodoro to improve your focus',
+                          icon: Icons.lock_clock_rounded),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: CardFeaturesList(
+                          title: 'Pomodoro',
+                          description:
+                              'Use this Pomodoro to improve your focus',
+                          icon: Icons.lock_clock_rounded),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: CardFeaturesList(
+                          title: 'Pomodoro',
+                          description:
+                              'Use this Pomodoro to improve your focus',
+                          icon: Icons.lock_clock_rounded),
+                    ),
+                  ],
                 ),
-                ElevatedButton(onPressed: () {
-                  Navigator.push(context, 
-                  MaterialPageRoute(builder: (context){
-                    return SubjectDetailsScreen();
-                  })
-                  );
-                } , child:Text('Go to details screen')) 
-         
-              ],
-            ),
+              )
+            ],
           ),
         ),
-      )
-    );
+      ),
+    ));
   }
 }
